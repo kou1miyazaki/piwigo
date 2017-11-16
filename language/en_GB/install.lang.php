98679a952672d61efc62bb7a01cf4a1ada634277
kou1miyazaki <kou1miyazaki@gmail.com>
2017-11-16 20:13:00 +0900

    First Commit.

---
+++ language/en_GB/install.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Congratulations, Piwigo installation is completed'] = 'Congratulations.  Piwigo installation is completed';
+$lang['Welcome to your new installation of Piwigo!'] = 'Welcome to your new installation of Piwigo!';
+$lang['Welcome to my photo gallery'] = 'Welcome to my photo gallery';
+$lang['Can\'t connect to server'] = 'Cannot connect to server';
+$lang['Password ']['confirm'] = 'Password [confirm]';
+$lang['Piwigo was not able to configure PHP 5.'] = 'Piwigo was not able to configure PHP 5.2';
+$lang['Sorry!'] = 'Sorry!';
+$lang['Start Install'] = 'Start installation';
+$lang['Try to configure PHP 5'] = 'Try to configure PHP 5.2';
+$lang['also given by your host provider'] = 'also supplied by your hosting provider';
+$lang['enter a login for webmaster'] = 'please enter the webmaster username';
+$lang['localhost or other, supplied by your host provider'] = 'localhost or other, supplied by your host provider';
+$lang['please enter your password again'] = 'please enter your password again';
+$lang['user login given by your host provider'] = 'user login supplied by your host provider';
+$lang['user password given by your host provider'] = 'user password supplied by your host provider';
+$lang['verification'] = 'verification';
+$lang['Keep it confidential, it enables you to access administration panel'] = 'Keep it confidential, it enables you to access the administration panel';
+$lang['Just another Piwigo gallery'] = 'Just another Piwigo gallery';
+$lang['It will be shown to the visitors. It is necessary for website administration'] = 'It will be shown to visitors. It is necessary for website administration';
+$lang['Installation'] = 'Installation';
+$lang['Host'] = 'Host';
+$lang['Hope to see you back soon.'] = 'Hope to see you back soon.';
+$lang['Download the config file'] = 'Download the config file';
+$lang['Database name'] = 'Database name';
+$lang['Database table prefix'] = 'Database tables prefix';
+$lang['Default gallery language'] = 'Default gallery language';
+$lang['Database configuration'] = 'Database configuration';
+$lang['Admin configuration'] = 'Administration configuration';
+$lang['Basic configuration'] = 'Basic configuration';
